d366c991ef36855fcb9b8ab614890928f95d1628
fitriasaridarmawan <fitriasaritaniadarmawan>
2022-06-29 10:42:43 +0700

    second commit 290622-1042

---
+++ app/Author.php
@@ -0,0 +1,19 @@
+<?php
+namespace App;
+use illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+
+    protected $fillable = [
+        'name', 'email', 'github', 'twitter', 'location', 'latest_article_published'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
+?>
